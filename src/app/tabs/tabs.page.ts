5037a0157952aa6304f9b358da5bf1911845a60b
boryeira <jmanuel.jorquera@gmail.com>
2020-03-03 12:12:28 -0300

    android

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,17 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //ionic
 import { Platform } from '@ionic/angular';
-//capacitor
-import {
-  Plugins,
-  PushNotification,
-  PushNotificationToken,
-  PushNotificationActionPerformed } from '@capacitor/core';
 
-const { PushNotifications } = Plugins;
-
-//services
-import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-tabs',
@@ -22,44 +12,10 @@ export class TabsPage implements OnInit {
 
   constructor(
     public platform: Platform,
-    private userService: UserService,
   ) { }
 
   ngOnInit() {
 
-    // Register with Apple / Google to receive push via APNS/FCM
-    PushNotifications.register();
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration', 
-      (token: PushNotificationToken) => {
-        alert('Push registration success, token: ' + token.value);
-        this.userService.addPushToken(token.value).subscribe(result => {
-          console.log(result);
-        });
-      }
-    );
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError', 
-      (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived', 
-      (notification: PushNotification) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed', 
-      (notification: PushNotificationActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
   }
 
 }
