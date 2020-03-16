ec325b97caaf9794edac07df2a8fcad5e4bb1db4
dandelior <jberrioscifuentes@gmail.com>
2020-03-16 18:27:01 -0300

    Spanish LOCALE Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { registerLocaleData } from '@angular/common';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -12,6 +12,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 //auth 
 import { AuthInterceptor } from '../app/services/auth/auth.interceptor';
+
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,6 +30,7 @@ import { AuthInterceptor } from '../app/services/auth/auth.interceptor';
     { provide: RouteReuseStrategy,
       useClass: IonicRouteStrategy 
     },
+    { provide: LOCALE_ID, useValue: 'es' },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
